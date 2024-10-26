68e3396cd830af303630b000a2c630a3a35ca127
LAB-USR-LCENTRO <LAB-USR-LCENTRO@grupoutp.net>
2024-10-26 09:46:10 -0500

    first commit

---
+++ src/componentes/Componente1.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Componente1 = ()=>{
+    return(
+        <div>
+            <h2>Componente Uno</h2>
+            <p>Mi primer parrafo en un componente</p>
+        </div>
+    )
+}
